1a0b98c9ad87fca21d4565cf3fc500c94cb74f6c
nobiart <dmitri.besleaga@gmail.com>
2025-02-21 21:59:46 +0200

    feat: add logo as image

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from "styled-components";
 import { styledTheme } from "./theme/styled-theme.ts";
 import { FontStyles, GlobalStyle } from "./theme/global-style.ts";
 import { AnimatedText } from "./components/AnimatedText.tsx";
+import LogoSVG from "./assets/images/logo.svg";
 
 function App() {
   return (
     <ThemeProvider theme={styledTheme}>
       <h1>Hello world</h1>
       <AnimatedText words={["CRM", "VOIP", "API"]} />
+      <img src={LogoSVG} alt="" />
       <GlobalStyle />
       <FontStyles />
     </ThemeProvider>
